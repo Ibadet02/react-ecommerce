6386e6faa62d6c38ccffd24c0586b79d86f05dc9
Ibadet <ibadetismayilov20@gmail.com>
2024-06-10 16:45:54 +0200

    enzyme updated

--- test/setup.js
+++ test/setup.js
@@ -1,8 +1,9 @@
 import DotEnv from 'dotenv';
 import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 DotEnv.config({ path: '.env.test' });
 Enzyme.configure({
-  adapter: new Adapter() 
+  adapter: new Adapter()
 });
+
